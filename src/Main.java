df252fda97efa7ba32d00aa066b1f67eb8fb8b01
Жевраков Руслан Александрович <zhevrakov.ra@att.edu.ru>
2022-11-10 13:24:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 0;
+        double b = 21;
+        double i;
+        i = b/a;
+        System.out.println(i);
+        }
+    }
